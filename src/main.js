91ead8b9ae9adcb247133b307a3d3384209f1086
Kevin Balanta <kevin232454@hotmail.com>
2020-03-10 19:30:06 -0500

    second commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import vuetify from './config/vuetify';
+import VueRouter from 'vue-router';
+
+import Home from './views/Home';
+import Pets from './views/Pets';
+
+Vue.config.productionTip=false;
+Vue.use(VueRouter)
+
+
+const routes= [
+  { path: "/", component:Home},
+  { path: "/pets", component:Pets}
+]
+let router = new VueRouter({routes})
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/views/Pets.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container grid-list-md fluid>
+      <v-row>
+          <v-col>
+              <v-card>
+                  <v-img src="https://goo.gl/6CQNDo">
+                        
+                      </v-img>
+                  <v-card-title>
+                      <div>
+                          <h3>
+                              Looking for a dog!
+                          </h3>
+                      </div>
+
+                  </v-card-title>
+              </v-card>
+          </v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+import { Dogs } from '../config/dog';
+export default {
+data(){
+    return {
+        dogs: Dogs
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
